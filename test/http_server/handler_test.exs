69c78f623bfa6c99df159b7bfb85d29b99f3ef4c
Laurent Curau <laurentcurau@gmail.com>
2018-09-18 11:16:53 +0100

    Simple GET with extra new line at the end

--- test/http_server/handler_test.exs
+++ test/http_server/handler_test.exs
@@ -17,4 +17,22 @@ defmodule HttpServer.Handler.Test do
 
     assert HttpServer.Handler.handle(request) == expected_response
   end
+
+  test "simple get request with new line at the end" do
+    request = """
+    GET /file2 HTTP/1.1
+
+    """
+
+    expected_response = """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 15
+
+    file2 contents
+
+    """
+
+    assert HttpServer.Handler.handle(request) == expected_response
+  end
 end
