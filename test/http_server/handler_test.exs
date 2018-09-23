78f3519d6b80b40dc11669d433d17671df2a00d8
Laurent Curau <laurentcurau@gmail.com>
2018-09-23 18:02:10 +0100

    Delete integration tests for GET

--- test/http_server/handler_test.exs
+++ test/http_server/handler_test.exs
@@ -1,58 +1,6 @@
 defmodule HttpServer.Handler.Test do
   use ExUnit.Case
 
-  test "simple get request" do
-    request = """
-    GET /file1 HTTP/1.1
-
-    """
-
-    expected_response = """
-    HTTP/1.1 200 OK
-    Content-Type: text/html
-    Content-Length: 14
-
-    file1 contents
-    """
-
-    assert HttpServer.Handler.handle(request) == expected_response
-  end
-
-  test "simple get request with new line at the end" do
-    request = """
-    GET /file2 HTTP/1.1
-
-    """
-
-    expected_response = """
-    HTTP/1.1 200 OK
-    Content-Type: text/html
-    Content-Length: 15
-
-    file2 contents
-
-    """
-
-    assert HttpServer.Handler.handle(request) == expected_response
-  end
-
-  test "GET request to non-existing file returns a 404" do
-    request = """
-    GET /no_file_here.txt HTTP/1.1
-
-    """
-
-    expected_response = """
-    HTTP/1.1 404 Not found
-    Content-Type: text/html
-    Content-Length: 14
-
-    File not found
-    """
-
-    assert HttpServer.Handler.handle(request) == expected_response
-  end
-
   test "HEAD request" do
     request = """
     HEAD / HTTP/1.1
