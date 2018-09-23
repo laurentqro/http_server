76b51c2ad29a9e034fdc3a969589143a432c8be2
Laurent Curau <laurentcurau@gmail.com>
2018-09-23 18:31:16 +0100

    HEAD request to non-existent file returns 404

--- lib/http_server/controllers/head.ex
+++ lib/http_server/controllers/head.ex
@@ -1,5 +1,21 @@
 defmodule HttpServer.Controllers.Head do
-  def head(conn) do
+  @public_dir "vendor/cob_spec/public"
+
+  def head(conn = %{ path: "/" }) do
     %{ conn | status: 200 }
+  end
+
+  def head(conn = %{ path: path }) do
+    @public_dir <> path
+    |> File.read
+    |> handle_file(conn)
+  end
+
+  defp handle_file({:ok, resp_body}, conn) do
+    %{ conn | resp_body: resp_body, status: 200 }
+  end
+
+  defp handle_file({:error, :enoent}, conn) do
+    %{ conn | resp_body: "File not found", status: 404 }
   end
 end
--- test/controllers/head_test.exs
+++ test/controllers/head_test.exs
@@ -7,4 +7,11 @@ defmodule HttpServer.Controllers.Head.Test do
 
     assert conn.status == 200
   end
+
+  test "HEAD request to non-existent file returns 404" do
+    conn = %{ path: "/head-request.txt", resp_body: "", status: "" }
+    conn = HttpServer.Controllers.Head.head(conn)
+
+    assert conn.status == 404
+  end
 end
